d86f536ab8639b8ed0fff651230d4b66b1c40adb
Kinniken <cedric@kstudio.net>
2013-04-13 16:28:52 +0200

    4.6.7

--- mill/org/millenaire/common/forge/Mill.java
+++ mill/org/millenaire/common/forge/Mill.java
@@ -112,7 +112,7 @@ public class Mill
 		{
 			return Mill.denier_or.itemID;
 		} 
-	}
+	} 
 
 	public static final String versionNumber = "4.6.7";
 	public static final String versionBound = "[4.5.1,5.0)";
