3f26903e6b512f233d2e02e4c8fa779fcbb8baf1
Kinniken <cedric@kstudio.net>
2013-04-21 16:51:54 +0200

    4.8.0

--- mill/org/millenaire/common/forge/Mill.java
+++ mill/org/millenaire/common/forge/Mill.java
@@ -114,8 +114,8 @@ public class Mill
 		} 
 	} 
 
-	public static final String versionNumber = "4.7.0";
-	public static final String versionBound = "[4.7.0,5.0)";
+	public static final String versionNumber = "4.8.0";
+	public static final String versionBound = "[4.8.0,5.0)";
 	public static final String modId="Millenaire";
 	public static final String name = "Mill\u00e9naire";
   
