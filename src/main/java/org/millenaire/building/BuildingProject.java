c082ddfb41f2aa7e83d037a81303dd8ae51612f7
Bosscrafter73 <l2600647@mvrht.com>
2017-02-16 22:16:31 -0600

    Added Test Building

--- src/main/java/org/millenaire/building/BuildingProject.java
+++ src/main/java/org/millenaire/building/BuildingProject.java
@@ -3,7 +3,9 @@ package org.millenaire.building;
 import java.util.ArrayList;
 
 import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.BlockStateBase;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 
 import org.millenaire.MillCulture;
@@ -14,7 +16,13 @@ public class BuildingProject
 	
 	public static void preinitialize()
 	{
-		normanCommunauteA0 = new BuildingPlan(MillCulture.normanCulture, 0).setNameAndType("Communauté", new String[]{"normanGuildMaster"}, new String[0]).setLengthWidth(11, 13).setHeightDepth(13, -6).setArea(3).setDistance(0, 1).setOrientation(EnumFacing.getHorizontal(2)).setPlan(
-				new IBlockState[][][]{{{}}});
+		IBlockState[][][] testbuilding = new IBlockState[7][7][7];
+		
+		for(int x = 0; x <= 7; x++) {
+			testbuilding[x][x][x] = Blocks.cobblestone.getDefaultState();
+		}
+		
+		normanCommunauteA0 = new BuildingPlan(MillCulture.normanCulture, 0).setNameAndType("Communauté", new String[]{"normanGuildMaster"}, new String[0]).setLengthWidth(11, 13).setHeightDepth(13, -6).setArea(3).setDistance(0, 1)
+				.setOrientation(EnumFacing.getHorizontal(2)).setPlan(testbuilding);
 	}
 }
