163501f8e715c75f57fe3a54d71dcb4364409095
Bosscrafter73 <l2600647@mvrht.com>
2017-02-19 10:50:36 -0600

    added some functionality to MillSigns

--- src/main/java/org/millenaire/items/ItemMillWand.java
+++ src/main/java/org/millenaire/items/ItemMillWand.java
@@ -214,7 +214,7 @@ public class ItemMillWand extends Item
 			((EntityMillVillager)entity).isPlayerInteracting = true;
 			
 			NBTTagCompound nbt = new NBTTagCompound();
-			player.getHeldItem().setTagCompound(nbt);
+			player.getHeldItem().setTagCompound(nbt); 
 			nbt.setInteger("ID", entity.getEntityId());
 			
 			if(player.worldObj.isRemote)
